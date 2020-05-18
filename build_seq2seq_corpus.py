943ae5ae1e67b4463dc7262e009b02397fcb9a8c
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-18 13:00:44 +0200

    topicalchat with switched roles

--- build_seq2seq_corpus.py
+++ build_seq2seq_corpus.py
@@ -11,21 +11,55 @@ class Turn(NamedTuple):
     response: str
 
 
+def get_limited_history(history: List, k: int, hist_len: int):
+    return history[max(0, k - hist_len) : (k + 1)]
+
+
 def generate_coqa_seq2seq(file_name, hist_len=3):
 
     file = os.environ["HOME"] + "/data/QA/coqa/" + file_name
     data = data_io.read_json(file)["data"]
 
-    def get_limited_history(l: List, k, hist_len):
-        return [d["input_text"] for d in l[max(0, k - hist_len) : (k + 1)]]
+    def get_history(l: List, k):
+        return [d["input_text"] for d in get_limited_history(l, k, hist_len)]
 
     for datum in data:
         dialogue_len = len(datum["questions"])
         for k in range(dialogue_len):
-            q_hist = get_limited_history(datum["questions"], k, hist_len)
-            a_hist = get_limited_history(datum["answers"], k, hist_len)
+            q_hist = get_history(datum["questions"], k)
+            a_hist = get_history(datum["answers"], k)
             turns = [Turn(req, res) for req, res in zip(q_hist, a_hist)]
             yield build_input_target(datum["story"], turns, SEP)
+
+
+def generate_topical_chat_seq2seq(file_name="train.json", hist_len=3):
+
+    file = (
+        os.environ["HOME"]
+        + "/code/DIALOGUE/alexa-prize-topical-chat-dataset/conversations/"
+        + file_name
+    )
+    data = list(data_io.read_json(file).values())
+
+    def build_turn(req, res):
+        assert req["agent"] != res["agent"]
+        return Turn(req["message"], res["message"])
+
+    def build_dialogues(utts):
+        assert len(utts) % 2 == 0
+        turns = [build_turn(utts[k], utts[k + 1]) for k in range(0, len(utts), 2)]
+        background = ""
+        for k in range(len(turns)):
+            some_turns = get_limited_history(turns, k, hist_len)
+            yield build_input_target(background, some_turns, SEP)
+
+    for datum in data:
+        utterances = datum["content"]
+        yield from build_dialogues(utterances[0 : len(utterances) // 2 * 2])
+        utterance_switched_roles = utterances[1:]
+        yield from build_dialogues(
+            utterance_switched_roles[0 : len(utterance_switched_roles) // 2 * 2]
+        )
 
 
 def generate_squad20_seq2seq(file_name):
@@ -76,6 +110,7 @@ SEP = tokenizer.special_tokens_map["sep_token"]
 if __name__ == "__main__":
     datagenerators = {
         "train": [
+            ("topicalchat-train", generate_topical_chat_seq2seq()),
             ("personachat-train", generate_personachat_seq2seq()),
             ("coqa-train", generate_coqa_seq2seq("coqa-train-v1.0.json")),
             ("squad20-train", generate_squad20_seq2seq("train-v2.0.json")),
@@ -96,3 +131,5 @@ if __name__ == "__main__":
                 for k, (x, y) in enumerate(g):
                     s.write(x + "\n")
                     t.write(y + "\n")
+                num_samples = k
+                print("%s: %d" % (name, num_samples))
